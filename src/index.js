1dcc2300c1752e8b074298d50537b5aa1b041f4f
arifrahman <matrep88@gmail.com>
2019-05-19 20:32:56 +0800

    added reducer

--- src/index.js
+++ src/index.js
@@ -7,12 +7,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <Provider store={{}}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
