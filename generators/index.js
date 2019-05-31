a93c96df94901e96fc585da28bcb155061f76d5c
arifrahman <matrep88@gmail.com>
2019-06-01 00:55:02 +0800

    update component generator

--- generators/index.js
+++ generators/index.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-// const componentGenerator = require('./component/index.js');
+const componentGenerator = require('./component/index.js');
 const containerGenerator = require('./container/index.js');
 // const routesGenerator = require('./routes/index.js');
 
 module.exports = plop => {
-  // plop.setGenerator('component', componentGenerator);
+  plop.setGenerator('component', componentGenerator);
   plop.setGenerator('container', containerGenerator);
   plop.addHelper('curly', (object, open) => (open ? '{' : '}'));
 };
