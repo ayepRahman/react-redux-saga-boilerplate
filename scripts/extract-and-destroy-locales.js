25eb439a7209f5dd908cdf239327e1019d20e4a7
arifrahman <matrep88@gmail.com>
2019-06-03 16:51:16 +0800

    added script to destroy

--- scripts/extract-and-destroy-locales.js
+++ scripts/extract-and-destroy-locales.js
@@ -1,2 +1,18 @@
 const jsonfile = require('jsonfile');
-import 
+const { languages } = require('../src/i18n/constants');
+
+function extractAndDestroy(lng) {
+  const file = `src/i18n/locales/${lng}/translations.json`;
+  console.log(file);
+
+  if (lng !== 'en') {
+    jsonfile.writeFile(file, {}, function(err, obj) {
+      if (err) console.error(err);
+      console.dir(obj);
+    });
+  }
+}
+
+for (var i = 0; i < languages.length; i++) {
+  extractAndDestroy(languages[i]);
+}
