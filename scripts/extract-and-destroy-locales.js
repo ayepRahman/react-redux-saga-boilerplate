95c59a956fbe82a24e8057b1eb794b80b556e70a
arifrahman <matrep88@gmail.com>
2019-06-03 16:54:37 +0800

    refactor code

--- scripts/extract-and-destroy-locales.js
+++ scripts/extract-and-destroy-locales.js
@@ -4,11 +4,11 @@ const { languages } = require('../src/i18n/constants');
 function extractAndDestroy(lng) {
   const file = `src/i18n/locales/${lng}/translations.json`;
   console.log(file);
+  console.log({ lng });
 
   if (lng !== 'en') {
-    jsonfile.writeFile(file, {}, function(err, obj) {
+    jsonfile.writeFile(file, {}, function(err) {
       if (err) console.error(err);
-      console.dir(obj);
     });
   }
 }
